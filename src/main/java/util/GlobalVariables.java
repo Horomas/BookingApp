4e498f26bdcb935148f39490254eeffa8427c02a
nilsgrintals <nils.grintals@testdevlab.com>
2024-04-10 20:38:13 +0300

    Android suite complete-corrected4, increased global wait time 20

--- src/main/java/util/GlobalVariables.java
+++ src/main/java/util/GlobalVariables.java
@@ -5,5 +5,5 @@ import java.time.Duration;
 public class GlobalVariables {
 
     public static final String localAppiumServerUrl = "http://localhost:4723";
-    public static final Duration globalTimeout = Duration.ofSeconds(12);
+    public static final Duration globalTimeout = Duration.ofSeconds(20);
 }
